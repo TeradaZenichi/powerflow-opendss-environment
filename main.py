fa65b0175cdb4934b814473d5155f1b1d6f2b3c3
a239526 <a239526@dac.unicamp.br>
2026-08-14 13:20:35 -0300

    add support for both regular simulation (main.py) and env simulation (main_env.py)

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ from opendss.results import simulation_results
 
 if __name__ == "__main__":
 
+    # Select the case to run the simulation
     case = "case5"
 
-    episodes = 1
-    episode_steps = 24
-
-    simulation_results(
-        case,
-        episodes,
-        episode_steps,
-    )
+    # Run the simulation and save the results
+    simulation_results(case)
